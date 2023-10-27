ecc7e367d96daa040c49dd2705a97c18ea17ac1a
supershopping <wenliowen@gmail.com>
2023-10-27 08:56:37 -0400

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OxyPlotPrintTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public PlotModel MyModel { get; private set; }
+
+        public MainWindow()
+        {
+            MyModel = new PlotModel { Title = "Example 1" };
+            MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Visual visual = Main as Visual;
+            Print(visual);
+        }
+
+        private void Print(Visual v)
+        {
+
+            System.Windows.FrameworkElement e = v as System.Windows.FrameworkElement;
+
+            if (e == null)
+                return;
+
+            PrintDialog pd = new PrintDialog();
+            pd.PrintTicket.PageOrientation = System.Printing.PageOrientation.Landscape;
+            if (pd.ShowDialog() == true)
+            {
+                //store original scale
+                Transform originalScale = e.LayoutTransform;
+                //get selected printer capabilities
+                System.Printing.PrintCapabilities capabilities = pd.PrintQueue.GetPrintCapabilities(pd.PrintTicket);
+
+                //get scale of the print wrt to screen of WPF visual
+                double scale = Math.Min(capabilities.PageImageableArea.ExtentWidth / e.ActualWidth, capabilities.PageImageableArea.ExtentHeight /
+                               e.ActualHeight);
+
+                //Transform the Visual to scale
+                e.LayoutTransform = new ScaleTransform(scale, scale);
+
+                //get the size of the printer page
+                System.Windows.Size sz = new System.Windows.Size(capabilities.PageImageableArea.ExtentWidth, capabilities.PageImageableArea.ExtentHeight);
+
+                //update the layout of the visual to the printer page size.
+                e.Measure(sz);
+                e.Arrange(new System.Windows.Rect(new System.Windows.Point(capabilities.PageImageableArea.OriginWidth, capabilities.PageImageableArea.OriginHeight), sz));
+
+                //now print the visual to printer to fit on the one page.
+                pd.PrintVisual(v, "My Print");
+
+                //apply the original transform.
+                e.LayoutTransform = originalScale;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            plot.Model = MyModel;
+        }
+    }
+
+}
